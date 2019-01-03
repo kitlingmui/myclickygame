372e51486f98a183b4d434fdaaa96a7c2125e9ff
kitlingmui <kitlingmui@gmail.com>
2019-01-03 02:32:41 -0800

    updateV3

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <div>
             <Navbar/>  
             <Route exact path='/' component={Home} />
-            <Route exact path='/myclickygame' component={Home} />
+            <Route path='/myclickygame' component={Home} />
             <Route path='/game' component={Game} />
           </div>
         </Router>
