0eef80a8c12fa4408fc29d60e81d4d107f4bed18
kitlingmui <kitlingmui@gmail.com>
2019-01-03 02:22:51 -0800

    Updates

---
+++ precache-manifest.0b5326a79450969395c4b35eb96ccc46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4117702114b9243db29d",
+    "url": "/myclickygame/static/js/runtime~main.41177021.js"
+  },
+  {
+    "revision": "dffbfe748cd64966f35b",
+    "url": "/myclickygame/static/js/main.dffbfe74.chunk.js"
+  },
+  {
+    "revision": "ce37f86e0bdc92adba8b",
+    "url": "/myclickygame/static/js/1.ce37f86e.chunk.js"
+  },
+  {
+    "revision": "dffbfe748cd64966f35b",
+    "url": "/myclickygame/static/css/main.1c139f24.chunk.css"
+  },
+  {
+    "revision": "918b1beaa685a09f09625b2291cda17c",
+    "url": "/myclickygame/index.html"
+  }
+];
